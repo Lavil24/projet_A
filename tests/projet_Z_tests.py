415e1d6eb002e78c5495edf492e775a1d1729989
Lavil24 <paul.ddlvdf@gmail.com>
2024-12-12 14:44:46 +0100

    ajout dossier vscode

---
+++ tests/projet_Z_tests.py
@@ -0,0 +1,36 @@
+# tests/test_main.py
+
+import pytest
+import sys
+import os
+
+sys.path.append(sys.path.abspath(sys.path.join(sys.path.dirname(__file__),'../src')))
+
+from src.projet_z.projet_Z_main import addition, soustraction, multiplication, division, division_euclidienne
+
+def test_addition():
+    assert addition(3, 2) == 5
+    assert addition(-1, 1) == 0
+    assert addition(0, 0) == 0
+
+def test_soustraction():
+    assert soustraction(3, 2) == 1
+    assert soustraction(2, 3) == -1
+    assert soustraction(0, 0) == 0
+
+def test_multiplication():
+    assert multiplication(3, 2) == 6
+    assert multiplication(-1, 1) == -1
+    assert multiplication(0, 5) == 0
+
+def test_division():
+    assert division(6, 2) == 3
+    assert division(5, 2) == 2.5
+    with pytest.raises(ZeroDivisionError):
+        division(1, 0)
+
+def test_division_euclidienne():
+    assert division_euclidienne(6, 2) == 3
+    assert division_euclidienne(5, 2) == 2
+    with pytest.raises(ZeroDivisionError):
+        division_euclidienne(1, 0)
